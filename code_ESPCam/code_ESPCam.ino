a02098606773ea71a5dce851b80f19a3aa78c8c5
HuYingTran <huynh.technical@gmail.com>
2024-09-18 14:19:24 +0700

    v1

--- code_ESPCam/code_ESPCam.ino
+++ code_ESPCam/code_ESPCam.ino
@@ -1,94 +1,165 @@
 #include "esp_camera.h"
 #define CAMERA_MODEL_AI_THINKER
 #include "camera_pins.h"
-
 #include "BluetoothSerial.h"
-#if !defined(CONFIG_BT_SPP_ENABLED)
-#error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
-#endif
+#include "Base_64.h"
 
-#define uS_TO_S_FACTOR 1000000ULL /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  5          /* Time ESP32 will go to sleep (in seconds) */
-RTC_DATA_ATTR int bootCount = 0;
-
-bool captureted = false;
-
-BluetoothSerial SerialBT;
-String slaveName = "ESP32-BT-Slave"; // Change this to reflect the real name of your slave BT device
+uint8_t SMA[6] = {0xCC, 0xDB, 0xA7, 0x3F, 0x6F, 0x6A}; // address MAC ESP32-Wifi
 String myName = "ESP32-BT-Master";
+BluetoothSerial SerialBT;
 
-void setup() {
+#define uS_TO_S_FACTOR 3600000000 // ms->hour
+
+void setup()
+{
   Serial.begin(115200);
+
+  initCamera();
+  delay(500);
+
   BT_init();
   BT_connect();
-  initCamera();
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
 }
 
-void esp_cam_sleep(){
-  Serial.println("Going to sleep now");
-  Serial.flush();
-  esp_deep_sleep_start();
-}
-
-void BT_init(){
-  if(!SerialBT.begin(myName)){
+void BT_init()
+{
+  if (!SerialBT.begin(myName, true))
+  {
     Serial.println("An error occurred initializing Bluetooth");
     ESP.restart();
-  }else{
+  }
+  else
+  {
     Serial.println("Bluetooth initialized");
   }
   SerialBT.register_callback(btCallback);
-  Serial.println("The device started, now you can pair it with bluetooth");
 }
 
-void BT_connect(){
-  int connected = SerialBT.connect(slaveName);
-  Serial.printf("Connecting to slave BT device named \"%s\"\n", slaveName.c_str());
-  if(connected) {
+void BT_connect()
+{
+  bool connected;
+  connected = SerialBT.connect(SMA);
+  if (connected)
+  {
     Serial.println("Connected Successfully!");
-  } else {
-    while(!SerialBT.connected(10000)) {
-      Serial.println("Failed to connect. Make sure remote device is available and in range, then restart app.");
-    }
+  }
+  else
+  {
+    Serial.println("Can't Connect!");
+    ESP.restart();
   }
 }
 
-void btCallback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
-  if(event == ESP_SPP_SRV_OPEN_EVT){
-    Serial.println("Client Connected!");
-  }else if(event == ESP_SPP_DATA_IND_EVT){
-    Serial.printf("ESP_SPP_DATA_IND_EVT len=%d, handle=%d\n\n", param->data_ind.len, param->data_ind.handle);
+void btCallback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
+{
+  if (event == ESP_SPP_SRV_OPEN_EVT)
+  {
+    Serial.println("Connected!");
+  }
+  else if (event == ESP_SPP_DATA_IND_EVT)
+  {
     String stringRead = String(*param->data_ind.data);
-    int paramInt = stringRead.toInt() - 48;
-    Serial.printf("paramInt: %d\n", paramInt);
-    capture();
+    int hour = stringRead.toInt() - 48;
+    send_image_robot_flow(hour);
+    calculate_time_sleep(hour);
   }
 }
 
-void capture(){
+String Photo2Base64()
+{
   camera_fb_t *fb = NULL;
-  esp_err_t res = ESP_OK;
   fb = esp_camera_fb_get();
-  if(!fb){
-    esp_camera_fb_return(fb);
-    return;
+  if (!fb)
+  {
+    Serial.println("Camera capture failed");
+    return "";
   }
-
-  if(fb->format != PIXFORMAT_JPEG){
-    return;
+  String imageFile = "";
+  char *input = (char *)fb->buf;
+  char output[base_64_enc_len(3)];
+  for (int i = 0; i < fb->len; i++)
+  {
+    base_64_encode(output, (input++), 3);
+    if (i % 3 == 0)
+      imageFile += urlencode(String(output));
   }
-  writeSerialBT(fb);
   esp_camera_fb_return(fb);
-  captureted = true;
+  esp_camera_deinit();
+  return imageFile;
 }
 
-void writeSerialBT(camera_fb_t *fb){
-  SerialBT.write(fb->buf, fb->len);
+String urlencode(String str)
+{
+  const char *msg = str.c_str();
+  const char *hex = "0123456789ABCDEF";
+  String encodedMsg = "";
+  while (*msg != '\0')
+  {
+    if (('a' <= *msg && *msg <= 'z') || ('A' <= *msg && *msg <= 'Z') || ('0' <= *msg && *msg <= '9') || *msg == '-' || *msg == '_' || *msg == '.' || *msg == '~')
+    {
+      encodedMsg += *msg;
+    }
+    else
+    {
+      encodedMsg += '%';
+      encodedMsg += hex[(unsigned char)*msg >> 4];
+      encodedMsg += hex[*msg & 0xf];
+    }
+    msg++;
+  }
+  return encodedMsg;
+}
+
+void capture()
+{
+  Serial.println("Encode base64");
+  String encodedString = Photo2Base64();
+  Serial.println("Send image base64 by Bluetooth");
+  SerialBT.println(encodedString);
   SerialBT.flush();
 }
 
-void initCamera(){
+void send_image_robot_flow(int time_now)
+{
+  if (time_now == 6 || time_now == 11 || time_now == 16)
+  {
+    initCamera();
+    delay(500);
+
+    capture();
+    delay(1000);
+  }else{
+    Serial.println("It's not time to work yet");
+  }
+  delay(100);
+}
+
+void calculate_time_sleep(int time_now)
+{
+  int time_sleep = 0;
+  if (time_now < 6)
+  {
+    time_sleep = 6 - time_now;
+  }
+  else if (time_now < 11)
+  {
+    time_sleep = 11 - time_now;
+  }
+  else if (time_now < 16)
+  {
+    time_sleep = 16 - time_now;
+  }
+  else
+  {
+    time_sleep = 6 + 24 - time_now;
+  }
+  esp_sleep_enable_timer_wakeup(time_sleep * uS_TO_S_FACTOR);
+  delay(100);
+  esp_deep_sleep_start();
+}
+
+void initCamera()
+{
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -110,12 +181,15 @@ void initCamera(){
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  //init with high specs to pre-allocate larger buffers
-  if(psramFound()){
+  // init with high specs to pre-allocate larger buffers
+  if (psramFound())
+  {
     config.frame_size = FRAMESIZE_UXGA;
     config.jpeg_quality = 10;
     config.fb_count = 2;
-  } else {
+  }
+  else
+  {
     config.frame_size = FRAMESIZE_SVGA;
     config.jpeg_quality = 12;
     config.fb_count = 1;
@@ -123,16 +197,11 @@ void initCamera(){
 
   // camera init
   esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
+  if (err != ESP_OK)
+  {
     Serial.printf("Camera init failed with error 0x%x", err);
     ESP.restart();
   }
 }
 
-void loop() {
-  if(captureted){
-    captureted = false;
-    delay(1000);
-    esp_cam_sleep();
-  }
-}
+void loop(){}
--- code_ESPWifi/code_ESPWifi.ino
+++ code_ESPWifi/code_ESPWifi.ino
@@ -1,19 +1,8 @@
-#include "esp_system.h"
-#include "base64.h"
-#include "SPIFFS.h"
-
 #include "BluetoothSerial.h"
-
-#include <WiFi.h>
+#include "WiFi.h"
+#include "time.h"
 #include <HTTPClient.h>
-
-char str_arr[4096]={0};
-
-const char* ssid = "HuynhTran";
-const char* password = "Huynh123";
-const char* serverName = "https://detect.roboflow.com/esp32-czges/1?api_key=4NITBHY9LYoMI89e45kx";
-
-String imageBase64;
+#include <ArduinoJson.h>
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -21,75 +10,151 @@ String imageBase64;
 #if !defined(CONFIG_BT_SPP_ENABLED)
 #error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
 #endif
+
 // Bluetooth Serial Object (Handle)
 BluetoothSerial SerialBT;
 String device_name = "ESP32-BT-Slave";
- 
-void setup() {
+
+const char *ssid = "HuynhTran";
+const char *password = "Huynh123";
+const char *serverName = "https://detect.roboflow.com/esp32-czges/1?api_key=4NITBHY9LYoMI89e45kx";
+const char *ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 25200; //GMT+7 => 3600*7=25200
+const int daylightOffset_sec = 0;
+
+float ripeness = 0;
+
+void setup()
+{
   Serial.begin(115200);
+  // Kết nối WiFi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
   }
-  SerialBT.begin(device_name); //Bluetooth device name
-//  spiff_read();
-//  http_post();
+  Serial.println("");
+  Serial.println("WiFi connected.");
+
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  bt_Init();
 }
 
-void BT_read(){
-  if (SerialBT.available()) {  // Kiểm tra nếu có dữ liệu qua Bluetooth
-    uint8_t buffer[bufferSize]; // Tạo bộ đệm để nhận dữ liệu
-    int bytesRead = SerialBT.readBytes(buffer, bufferSize);  // Đọc dữ liệu từ Bluetooth
-  
-    Serial.print("Nhận được ");
-    Serial.print(bytesRead);
-    Serial.println(" byte:");
-    
-    for (int i = 0; i < bytesRead; i++) {
-      Serial.print(buffer[i], HEX);  // In từng byte nhận được dưới dạng HEX
-      Serial.print(" ");
+void bt_Init(){
+  SerialBT.begin(device_name); // Bluetooth device name
+  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+  SerialBT.register_callback(btCallback);
+}
+
+void btCallback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
+{
+  if (event == ESP_SPP_SRV_OPEN_EVT)
+  {
+    Serial.println("Client Connected!");
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo))
+    {
+      Serial.println("Failed to obtain time");
+      ESP.restart();
     }
-    Serial.println();
+    SerialBT.println(&timeinfo, "%H");
+    SerialBT.flush();
   }
 }
 
-void http_post() {
+void http_post(String raw)
+{
   HTTPClient http;
   http.begin(serverName);
-  http.addHeader("Content-Type","application/x-www-form-urlencoded");
-  int httpResponseCode = http.POST(imageBase64);
-  if (httpResponseCode > 0) {
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int httpResponseCode = http.POST(raw);
+  if (httpResponseCode > 0)
+  {
     String response = http.getString();
     Serial.println(httpResponseCode);
     Serial.println(response);
-  } else {
+    json_data(response);
+  }
+  else
+  {
     Serial.print("Error on sending POST: ");
     Serial.println(httpResponseCode);
   }
   http.end();
 }
 
-void spiff_read(){
-  if(!SPIFFS.begin(true)){
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
-  String str;
-  File file = SPIFFS.open("/image_test.txt");
-  if(!file){
-    Serial.println("Failed to open file for reading");
+void json_data(String response)
+{
+  StaticJsonDocument<2048> doc;
+  DeserializationError error = deserializeJson(doc, response);
+
+  if (error)
+  {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
     return;
   }
 
-  while (file.available()) {
-    imageBase64 += (char)file.read();
+  JsonArray predictions = doc["predictions"];
+  int greenCount = 0;
+  int ripeCount = 0;
+
+  for (JsonObject prediction : predictions)
+  {
+    const char *classType = prediction["class"];
+
+    // Ckeck class and counter
+    if (strcmp(classType, "green") == 0)
+    {
+      greenCount++;
+    }
+    else if (strcmp(classType, "unripe") == 0)
+    {
+      ripeCount++;
+    }
+    else if (strcmp(classType, "ripe") == 0)
+    {
+      ripeCount++;
+    }
   }
-  file.close();
-  Serial.println(imageBase64);
-  
+  int sum = ripeCount + greenCount;
+  if(ripeCount){
+    ripeness = ripeCount / (ripeCount + greenCount);
+  }else{
+    ripeness = 0;
+  }
+
+  // In kết quả ra Serial Monitor
+  Serial.print("Total green: ");
+  Serial.println(greenCount);
+  Serial.print("Total ripe: ");
+  Serial.println(ripeCount);
+  Serial.print("ripeness of fruit");
+  Serial.println(ripeness);
 }
- 
-void loop() {
-   BT_read();
+
+int bt_wait = 0;
+void loop()
+{
+  String data_bt = "";
+  while (SerialBT.available()) {
+    bt_wait = 1;
+    char c = SerialBT.read();    // Đọc một ký tự từ SerialBT
+    data_bt += c;
+  }  
+
+  if (bt_wait && data_bt > "can_not")
+  {
+    bt_wait = 0;
+    delay(2000);
+    SerialBT.end();
+    Serial.println("Http post Robotflow");
+    http_post(data_bt);
+    delay(1000);
+    bt_Init();
+  }
+  delay(1000);
 }
